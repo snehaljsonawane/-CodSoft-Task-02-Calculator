4eb95d237e41ffb169191572ad4783eee2ee8640
snehaljsonawane <snehaljsonawane25@gmail.com>
2025-09-14 12:43:47 +0530

    Add files via upload
    A beginner-friendly calculator built using Python that performs basic arithmetic operations like addition, subtraction, multiplication, and division.
    This project emphasizes user input handling, error checking, and clean output formatting—essential skills for any developer.
    🧠 Highlights:
    - Interactive CLI design
    - Input validation and exception handling
    - Modular code structure for easy expansion
    🎯 Outcome: A reliable calculator that lays the groundwork for more advanced math-based applications.

---
+++ task_2.py
@@ -0,0 +1,20 @@
+#def calculator():
+ #   op = input("Choose operation (+ - * /): ")
+ #   num1 = float(input("Enter first number: "))
+ #   num2 = float(input("Enter second number: "))
+
+  #  if op == '+':
+  #      print("Result:", num1 + num2)
+  #  elif op == '-':
+  #      print("Result:", num1 - num2)
+   # elif op == '*':
+   #     print("Result:", num1 * num2)
+   # elif op == '/':
+    #    if num2 != 0:
+     #       print("Result:", num1 / num2)
+     #   else:
+     #       print("Cannot divide by zero.")
+   # else:
+    #    print("Invalid operation.")
+
+#calculator()
